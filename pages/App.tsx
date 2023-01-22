289e993359c4f39d7c147a600651626970a23d17
Rodney Brown <85382370+linothecreator@users.noreply.github.com>
2023-01-22 22:33:01 +0000

    Initial commit

---
+++ pages/App.tsx
@@ -0,0 +1,16 @@
+import Navbar from '/components/Navbar.jsx';
+import Hero_Section from '/components/Hero_Section.jsx'
+import Footer from '/components/Footer.jsx'
+export default function App () {
+  return (
+    <>
+      <Navbar/>
+      <img src="https://drive.google.com/file/d/1OsNFqCAwG657Q-G4dnwf9sgMQLk8ZgsK/view?usp=sharing"></img>
+      <Hero_Section/>
+      <Footer/>
+    
+    
+    </>
+  )
+}
+
---
+++ pages/index.tsx
@@ -0,0 +1,16 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+import App from './App.tsx'
+
+const Home: NextPage = () => {
+  return (
+    <>
+      <App/>
+    
+    
+    </>
+  )
+}
+
+export default Home
